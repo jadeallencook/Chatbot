7715d4aea348f1e455b1791a04c3a9576bbbada9
Jade Allen Cook <jcook@deseretnews.com>
2018-02-24 17:30:32 -0500

    ⚙️ First init working model with JSON

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,79 @@
-(function() {
-  console.log(true);
+(function () {
+  // jquery, lol
+  function $(elem) {
+    var elem = document.querySelectorAll(elem);
+    if (elem.length === 1) return elem[0];
+    else return elem;
+  }
+  var messages = {
+    elem: $('div.questions-bot-container ul'),
+    questions: data,
+    cue: [],
+    init: false,
+    answer: function (num) {
+      var question = this.cue[num];
+      this.elem.innerHTML = '';
+      var questionElem = document.createElement('li');
+      questionElem.classList.add('ask');
+      questionElem.innerText = question.question;
+      questionElem.classList.add('animated');
+      questionElem.classList.add('fadeIn');
+      var answerElem = document.createElement('li');
+      answerElem.classList.add('answer');
+      answerElem.innerText = question.answer;
+      answerElem.classList.add('animated');
+      answerElem.classList.add('fadeIn');
+      this.elem.appendChild(questionElem);
+      this.elem.appendChild(this.blank());
+      this.elem.appendChild(answerElem);
+      this.elem.appendChild(this.blank());
+      this.load();
+    },
+    blank: function () {
+      return document.createElement('li');
+    },
+    clear: function () {
+      this.elem.innerHTML = '';
+    },
+    load: function () {
+      if (!this.init) {
+        for (var x = 0; x < 4; x++) {
+          if (this.questions[x]) this.cue.push(this.questions[x]);
+        }
+      }
+      for (var x = 0, max = this.cue.length; x < max; x++) {
+        var question = this.cue[x];
+        var message = document.createElement('li');
+        message.classList.add('question');
+        message.classList.add('animated');
+        if (x % 2 == 0) message.classList.add('slideInLeft');
+        else message.classList.add('slideInRight');
+        message.innerText = question.question;
+        message.setAttribute('data-num', x);
+        this.elem.appendChild(message);
+        this.questions.splice(0, 1);
+        message.onclick = function () {
+          var num = this.getAttribute('data-num');
+          num = parseInt(num);
+          messages.answer(num);
+        }
+      }
+      // bottom message
+      var disclaimer = document.createElement('li');
+      disclaimer.classList.add('msg');
+      disclaimer.classList.add('animated');
+      if (!this.init) {
+        disclaimer.classList.add('pulse');
+        disclaimer.classList.add('infinite');
+        disclaimer.innerText = 'Click a question to get started...';
+      } else {
+        disclaimer.classList.add('fadeIn');
+        disclaimer.innerText = 'What else would you like to know...';
+      }
+      this.elem.appendChild(disclaimer);
+      this.init = true;
+    }
+  }
+  messages.clear();
+  messages.load();
 })();
---
+++ assets/data.js
@@ -0,0 +1,39 @@
+const data = [
+  {
+    question: "How do I customize question bot?",
+    answer: "You can download question bot's source code on GitHub and then you just have to generate your own JSON!",
+    next: {
+      question: "My next question",
+      answer: "This is my next answer!",
+      next: false
+    }
+  }, {
+    question: "What is question bot about?",
+    answer: "Question bot makes it easy and fun for your users to interact with your content. Not only will it increase engagment time but it also helps your readers learn more!",
+    next: false
+  }, {
+    question: "Who made question bot?",
+    answer: "This bot was made by Jade Allen Cook, look at the footer!",
+    next: false
+  }, {
+    question: "What is the meaning of life?",
+    answer: "Idk, I didn't make it.",
+    next: false
+  }, {
+    question: "Another question 4",
+    answer: "Another answer",
+    next: false
+  }, {
+    question: "Another question 5",
+    answer: "Another answer",
+    next: false
+  }, {
+    question: "Another question 6",
+    answer: "Another answer",
+    next: false
+  }, {
+    question: "Another question 7",
+    answer: "Another answer",
+    next: false
+  }
+]
